471ffa6d65a3f33cf4263a46a5144da91d6e3171
cbsouza20 <2025363@student.cct.ie>
2026-05-16 11:27:59 +0100

    Creating Department class

---
+++ CA_2/src/main/java/com/mycompany/ca_2/Department.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ca_2;
+
+/**
+ * Department class used to store department information for each employee in
+ * the organization system.
+ * @author Camila
+ */
+public class Department {
+
+    //Store the department name
+    String departmentName;
+
+    //Contructor initializes the department name
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+}
