5042ca6c10f2aa27fb9eb3a31c6192b4dfb5882b
cbsouza20 <2025363@student.cct.ie>
2026-05-16 11:27:31 +0100

    Creating Manager class

---
+++ CA_2/src/main/java/com/mycompany/ca_2/Manager.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ca_2;
+
+/**
+ * Manager class used to store manager information. Defines the manager role
+ * type in the organization.
+ *
+ * @author Camila
+ */
+public class Manager {
+
+    //Stores manager type
+    String managerType;
+
+    //Constructor initializes manager type
+    public Manager(String managerType) {
+        this.managerType = managerType;
+    }
+
+}
