26f5d33d1fbd666007ca6c60af1d3d4e1586093a
cbsouza20 <2025363@student.cct.ie>
2026-05-16 10:38:33 +0100

    Creating project

---
+++ CA_2/src/main/java/com/mycompany/ca_2/CA_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ca_2;
+
+/**
+ *
+ * @author Camila
+ */
+public class CA_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
